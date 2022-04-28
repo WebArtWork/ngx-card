c46ef8e2f4dc7ad34c0cb5b0dcfcc2150eab4748
Oganesman <96392987+qweRTyiOp123789@users.noreply.github.com>
2022-04-28 15:58:22 +0300

    Add files via upload

--- card.component.ts
+++ card.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 @Component({
 	selector: 'wcard',
 	templateUrl: './card.component.html',
 	styleUrls: ['./card.component.scss']
 })
 export class CardComponent {
+	@Input() align = 1;
 	constructor() { }
 }
