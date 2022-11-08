53f6cd93a316b2220caa593f779a96e1b5e7406a
Honchar Denys <crackeraki@gmail.com>
2022-11-08 02:09:59 +0200

    improved card component

--- card.component.ts
+++ card.component.ts
@@ -1,10 +1,20 @@
 import { Component, Input } from '@angular/core';
+
 @Component({
 	selector: 'wcard',
 	templateUrl: './card.component.html',
 	styleUrls: ['./card.component.scss']
 })
 export class CardComponent {
-	@Input() align = {};
-	constructor() { }
+	@Input() padding = false;
+
+	@Input() align = 'center';
+
+	@Input() image: boolean;
+
+	@Input() title: string;
+
+	@Input() subtitle: string;
+
+	@Input() text: string;
 }
